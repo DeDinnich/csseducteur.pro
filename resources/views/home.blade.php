ab0fa0f15ce1dbf77d04ef1f77a9e0b65e7da678
DeDinnich <lucas.dinnichert.pro#gmail.com>
2024-12-12 16:37:32 +0100

    ajout de correctifs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,7 @@
         <div class="home_text">
             <h1>Trouve le CsSéducteur qui partagera la carriere</h1>
         </div>
-        <div class="home_button">
-        <a href="{{ route('search') }}" class="start-button">Commencer</a>
-        </div>
+        <a href="{{ route('search') }}" class="home_button">Commencer</a>
         <div class="home_img">
         @foreach ($frameworks as $index => $framework)
         <!-- Image en base64 -->
